068cc038072ce3940d88dc22d00513a766cf3eb4
Pabl0Hernandez <pablo.hernandez@aluno.senai.br>
2026-02-12 16:59:39 -0300

    feat: atualizar index.html e refatorar App.jsx com novos componentes Link, SocialLink e Rodape

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,22 +1,31 @@
-import './App.css';
-import Perfil from './components/Perfil/Perfil';
+import "./App.css";
+import Link from "./components/Link/Link";
+import Perfil from "./components/Perfil/Perfil";
+import SocialLink from "./components/SocialLink/SocialLink";
+import Rodape from "./components/Rodape/Rodape";
+
 
 function App() {
   return (
-    <div id='App'>
+    <div id="App">
+    <Perfil fotoPerfil={"https://placehold.co/100"}> Pablo Hernandez シ </Perfil> 
 
-    <Perfil />
+      <div className="switch"> botão switch </div>
 
-      <div className='perfil'>
-      <img src="(https://placehold.co/200)" alt="" />
-      <p>@Seu Nome</p>
+      <ul>
+        <Link url={""}>Increva-se</Link>
+        <Link url={""}>Minha Playlist</Link>
+        <Link url={""}>Me pague um café!</Link>
+        <Link url={""}>Conheça o Curso DEV</Link>
+      </ul>
+      <div className="socialLinks">
+      <SocialLink url={"https://github.com"} icon={"logo-github"} /> 
+      <SocialLink url={"https://instagram.com"} icon={"logo-instagram"} /> 
+      <SocialLink url={"https://youtube.com"} icon={"logo-youtube"} /> 
+      <SocialLink url={"https://linkd.in"} icon={"logo-linkedin"} /> 
       </div>
-      <div className='switch'> botão switch </div>
-      <div className='links'></div>
-      <div className='socilLinks'></div>
-      <div className='rodape'></div>
-    </div>
+      <Rodape>PabloH</Rodape>
+      </div>
   )
 }
-
-export default App
+export default App  
