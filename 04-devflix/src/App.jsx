6ccce05112bd604391dff630c991b7ea98a3e03f
Pabl0Hernandez <pablo.hernandez@aluno.senai.br>
2026-02-19 17:05:04 -0300

    feat: adicionar funcionalidade de pesquisa de filmes e criar componentes de exibição

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,13 +1,33 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import Rodape from "./assets/components/Rodape/Rodape";
 import logo from "./assets/devflix.png";
 import lupa from "./assets/search.svg";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  //Utilizando uma CHAVE de API do arquivo .env
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  const apiUrl = `https://ombdapi.com/?apikey=${apiKey}`;
+
+  // Criando a conexão com API e trazendo informações
+  const searchMovies = async (title) => {
+    const respone = await fetch(`${apiUrl}&s=${title}`);
+    const data = await respone.json;
+
+    // Alimentando a variavel movis
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
   return (
     <div id="App">
       <img
-        id="logo"
+        id="Logo"
         src={logo}
         alt="Logo do serviço de streaming DEVFLIX em destaque com cores vermelha e preta, representando uma plataforma de streaming de filmes e séries."
       />
@@ -18,7 +38,17 @@ const App = () => {
         <img src={lupa} alt="Botão de ação para pesquisa!" />
       </div>
 
-      <Rodape link={"https://github.com/Pabl0Hernandez"}>PabloHernandez</Rodape>
+<div className="container">
+  {movies.map((movies, index) => }
+</div>
+
+
+
+
+
+      <Rodape link={"https://github.com/Pabl0Hernandez"}>
+        PabloHernandez シ{" "}
+      </Rodape>
     </div>
   );
 };
