9c581dbf8368ed427d7f3092131779287b8d4e0a
Pabl0Hernandez <pablo.hernandez@aluno.senai.br>
2026-02-19 11:19:07 -0300

    feat: implementar componente Rodape e integrar pesquisa no App

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,9 +1,26 @@
 import "./App.css";
 
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div id="App">
+      <img
+        id="logo"
+        src={logo}
+        alt="Logo do serviço de streaming DEVFLIX em destaque com cores vermelha e preta, representando uma plataforma de streaming de filmes e séries."
+      />
 
-export default App
+      <div className="search">
+        <input type="text" placeholder="Pesquise por Filmes e Series..." />
+
+        <img src={lupa} alt="Botão de ação para pesquisa!" />
+      </div>
+      
+      <Rodape>Pablo Hernandez シ</Rodape>
+    </div>
+  );
+};
+
+export default App;
